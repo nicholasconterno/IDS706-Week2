81974d1e684636e3c20617d7824718517404eca8
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-09-06 11:34:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import pandas as pd
+url='https://gist.github.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea'
+df = pd.read_csv(url)
 
+print(df.describe())
+
+df.describe()
 def negative(x):
   return -x
 
