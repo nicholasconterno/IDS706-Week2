cc122a6a339ae3733118bba369703f5636bf8fc5
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-09-06 20:02:56 +0000

    Describe Movie csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
-url='https://gist.github.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea'
-df = pd.read_csv(url)
+url='https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv'
+df = pd.read_csv(url, skiprows=range(1, 32))  # Adjust the range as needed
+
 
 print(df.describe())
 
